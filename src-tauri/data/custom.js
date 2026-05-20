1c7ed2a70c82110c47e876a48a9030d49473cbc5
15184898912 <960116565@qq.com>
2026-05-20 12:17:47 +0800

    update custom js from pakeplus

--- src-tauri/data/custom.js
+++ src-tauri/data/custom.js
@@ -2,7 +2,7 @@ window.addEventListener("DOMContentLoaded",()=>{const t=document.createElement("
   "name": "我的离线工具",
   "title": "离线版",
   "exe": "离线工具.exe",
-  "url": "https://qyyckchzbm.coze.site",
+  "url": "https://9jvtrdz75w.coze.site",
   "width": 1280,
   "height": 720,
   "hideMenu": true,
